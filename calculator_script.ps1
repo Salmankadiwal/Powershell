550ba1ea8e390fce8599511ccfac3b3e469a9785
Salmank <77564543+Salmankadiwal@users.noreply.github.com>
2022-05-05 19:52:09 -0400

    Add files via upload

---
+++ calculator_script.ps1
@@ -0,0 +1,36 @@
+﻿#-----------------------Calculator Code--------------------------#
+
+[int]$num1=Read-host -Prompt 'Enter 1st Number'
+[char]$character=Read-host -Prompt 'Enter Operation You Want (+,-,*,/,%)'
+[int]$num2=Read-host -Prompt 'Enter 2nd Number'
+[int]$addition=$num1+$num2
+[int]$subtraction=$num1-$num2
+[int]$multiplication=$num1*$num2
+[double]$division=$num1/$num2
+[int]$modulus=$num1%$num2
+
+switch($character)
+{
+   +{$output = 'YOu Choose Addition'
+    Write-Host("Your answer is $addition")
+    break;
+    }
+   -{$output = 'YOu Choose Subtraction'
+    Write-Host("Your answer is $subtraction")
+    break;
+   }
+   *{$output = 'YOu Choose Multiplication'
+    Write-Host("Your answer is $multiplication")
+    break;
+   }
+   %{$output = 'YOu Choose Modulus'
+    Write-Host("Your answer is $modulus")
+    break;
+   }
+   /{$result = 'YOu Choose Divide'
+    Write-Host("Your answer is $division")
+    break;
+   }
+}
+$output
+pause
---
+++ powershell_datatype_script.ps1
@@ -0,0 +1,52 @@
+﻿cd\
+cd Users\salma\desktop
+New-Item powershell_file -ItemType Directory
+cd powershell_file
+echo "#---------------datatypes in powershell-------------------
+
+$a = 2
+$b = 220.25
+[char]$c = 0
+$d = "salman"
+
+$a.GetType().Name
+$b.GetType().Name
+$c.GetType().Name
+$d.GetType().Name
+
+#---------------variable in powershell-------------------
+
+$a 
+$sun_set
+
+#---------------operators in powershell-------------------
+
+<# assignment
+arithmetic
+logical
+comparison#>
+
+#arithmetic 
+$ad = 14
+$ds = 56
+$fd=$ad*$ds
+$fd
+
+#assignment 
+$ada = 14
+$dsa = 56
+$fda=$ad-=$ds
+$fda
+
+#logical
+$z = 7
+$x = 7
+($z -ge $x)
+
+#logical
+
+$o = 4
+$k = 7
+($o -gt $k) -! ($o -gt $k)
+" > datatype.txt
+notepad datatype.txt
